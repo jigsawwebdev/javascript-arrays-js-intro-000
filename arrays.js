ce7ff1ab75fbd73e1f7559895f8bb7215c781866
jigsawwebdev <devgaragedt@gmail.com>
2018-08-08 18:23:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,3 +36,10 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 accessElementInArray(array, 3)
+
+array = ['red', 'orange', 'yellow', 'green', 'blue', 'violet'];
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+destructivelyRemoveElementFromBeginningOfArray(array)
