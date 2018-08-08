9f896e8c053d6e0ff61022a12b72caa4afd28702
jigsawwebdev <devgaragedt@gmail.com>
2018-08-08 18:17:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,9 @@ function destructivelyAddElementToEndOfArray(x, y) {
   return x
 }
 destructivelyAddElementToEndOfArray(chocolateBars, 'Dove')
+
+array = ['red', 'orange', 'yellow', 'green', 'blue', 'violet'];
+function accessElementInArray(array, index) {
+  return array[index]
+}
+accessElementInArray(array, 3)
